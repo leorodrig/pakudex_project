b3843ac09288a7b52637b33628efa01f734e5bf4
leorodrig <leordrig@gmail.com>
2023-11-08 15:23:15 -0500

    pakudex class basic framework

--- pakudex.py
+++ pakudex.py
@@ -1,1 +1,25 @@
 # class describing the Pokédex as a whole
+class Pakudex:
+    def __init__(self, capacity=20):
+        self.capacity = capacity
+
+    def get_size(self):
+        pass
+
+    def get_capacity(self):
+        pass
+
+    def get_species_array(self):
+        pass
+
+    def get_stats(self, species):
+        pass
+
+    def sort_pakuri(self):
+        pass
+
+    def add_pakuri(self, species):
+        pass
+
+    def evolve_species(self, species):
+        pass
