6802f74cb4e12c58b1754141ab144e1b24ed343b
leorodrig <leordrig@gmail.com>
2023-11-09 11:09:47 -0500

    added 'add pakuri' method

--- pakudex.py
+++ pakudex.py
@@ -1,13 +1,20 @@
 # class describing the Pokédex as a whole
+# import pakuri
+from pakuri import Pakuri
+
+
 class Pakudex:
     def __init__(self, capacity=20):
         self.capacity = capacity
+        self.pakudex_obj_list = []
+        self.species_name_list = []
+        self.species_stored_count = 0
 
     def get_size(self):
         pass
 
     def get_capacity(self):
-        pass
+        return self.capacity
 
     def get_species_array(self):
         pass
@@ -19,7 +26,16 @@ class Pakudex:
         pass
 
     def add_pakuri(self, species):
-        pass
+        for name in self.species_name_list:
+            if name == species:
+                return False
+        if self.species_stored_count >= self.capacity:
+            return False
+        new_species = Pakuri(species)
+        self.pakudex_obj_list.append(new_species)
+        self.species_name_list.append(new_species)
+        self.species_stored_count += 1
+        return True
 
     def evolve_species(self, species):
         pass
